d371386685ce9367bbb4dbf293d1bef1769060f8
Joakim Plate <elupus@ecce.se>
2019-03-11 22:00:35 +0100

    Inital package

---
+++ README.rst
@@ -0,0 +1,22 @@
+********************************
+Arcam IP Control
+********************************
+
+
+Module
+======
+
+
+Status
+______
+.. image:: https://travis-ci.org/elupus/arcam_av.svg?branch=master
+    :target: https://travis-ci.org/elupus/arcam_av
+
+.. image:: https://coveralls.io/repos/github/elupus/arcam_av/badge.svg?branch=master
+    :target: https://coveralls.io/github/elupus/arcam_av?branch=master
+
+Console
+=======
+
+The module contains a commandline utility to test and request data from
+Nibe Uplink called ``arcam-av``.
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arcam-av',
+    version='0.1.0',
+    description='A python library for speaking to Arcam receivers',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    python_requires='>3.5',
+    author='Joakim Plate',
+    install_requires=[
+        'asyncio',
+        'attrs>18.1',
+    ],
+    extras_require={
+        'tests': [
+            'pytest>3.6.4',
+            'pytest-asyncio',
+            'pytest-cov<2.6',
+            'coveralls'
+        ]
+    },
+    entry_points = {
+        'console_scripts' : ['arcam-av=arcam_av:main']
+    },
+    url='https://github.com/elupus/arcam_av',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Framework :: AsyncIO',
+    ]
+)
