06d62403235feb540db237001737b00810332570
Joakim Plate <elupus@ecce.se>
2019-03-13 15:19:34 +0100

    Switch to event_loop instead of loop

--- tests/test_standard.py
+++ tests/test_standard.py
@@ -4,31 +4,31 @@ import arcam_av
 import pytest
 from unittest.mock import MagicMock, call
 
-async def test_reader_valid(loop):
-    reader = asyncio.StreamReader(loop=loop)
+async def test_reader_valid(event_loop):
+    reader = asyncio.StreamReader(loop=event_loop)
     reader.feed_data(b'\x21\x01\x08\x00\x02\x10\x10\x0D')
     reader.feed_eof()
     packet = await arcam_av._read_packet(reader)
     assert packet == arcam_av.ResponsePacket(1, 8 , 0, b'\x10\x10')
 
 
-async def test_reader_invalid_data(loop):
-    reader = asyncio.StreamReader(loop=loop)
+async def test_reader_invalid_data(event_loop):
+    reader = asyncio.StreamReader(loop=event_loop)
     reader.feed_data(b'\x21\x01\x08\x00\x02\x10\x0D')
     reader.feed_eof()
     with pytest.raises(arcam_av.InvalidPacket):
         await arcam_av._read_packet(reader)
 
 
-async def test_reader_short(loop):
-    reader = asyncio.StreamReader(loop=loop)
+async def test_reader_short(event_loop):
+    reader = asyncio.StreamReader(loop=event_loop)
     reader.feed_data(b'\x21\x10\x0D')
     reader.feed_eof()
     with pytest.raises(arcam_av.InvalidPacket):
         await arcam_av._read_packet(reader)
 
 
-async def test_writer_valid(loop):
+async def test_writer_valid(event_loop):
     writer = MagicMock()
     writer.write.return_value = None
     writer.drain.return_value = asyncio.Future()
