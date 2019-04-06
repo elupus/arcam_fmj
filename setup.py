1e78a853e7ea6a95d0a717a45392bcb180b74ed4
Joakim Plate <elupus@ecce.se>
2019-04-07 01:06:31 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='arcam-fmj',
-    version='0.1.0',
+    version='0.2.0',
     description='A python library for speaking to Arcam receivers',
     license='MIT',
     packages=find_packages('src'),
