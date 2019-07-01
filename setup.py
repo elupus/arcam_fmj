fc1601c36da8dda505468c970210bdb614802f44
Joakim Plate <elupus@ecce.se>
2019-07-01 13:32:39 +0200

    Alawys use " for strings (black formatting)

--- setup.py
+++ setup.py
@@ -2,45 +2,40 @@ from setuptools import find_packages, setup
 
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='arcam-fmj',
-    version='0.4.2',
-    description='A python library for speaking to Arcam receivers',
+    name="arcam-fmj",
+    version="0.4.2",
+    description="A python library for speaking to Arcam receivers",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    license='MIT',
-    packages=['arcam.fmj'],
-    package_dir={'': 'src'},
-    python_requires='>=3.6',
-    author='Joakim Plate',
-    install_requires=[
-        'asyncio',
-        'attrs>18.1',
-        'aionursery'
-    ],
+    long_description_content_type="text/x-rst",
+    license="MIT",
+    packages=["arcam.fmj"],
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    author="Joakim Plate",
+    install_requires=["asyncio", "attrs>18.1", "aionursery"],
     extras_require={
-        'tests': [
-            'pytest>3.6.4',
-            'pytest-asyncio',
-            'pytest-cov<2.6',
-            'coveralls',
-            'pytest-mock',
-            'asynctest'
+        "tests": [
+            "pytest>3.6.4",
+            "pytest-asyncio",
+            "pytest-cov<2.6",
+            "coveralls",
+            "pytest-mock",
+            "asynctest",
         ]
     },
-    entry_points={
-        'console_scripts':['arcam-fmj=arcam.fmj.console:main']
-    },
-    url='https://github.com/elupus/arcam_fmj',
+    entry_points={"console_scripts": ["arcam-fmj=arcam.fmj.console:main"]},
+    url="https://github.com/elupus/arcam_fmj",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Plugins',
-        'Framework :: AsyncIO',
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Plugins",
+        "Framework :: AsyncIO",
+    ],
 )
