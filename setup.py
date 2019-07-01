62cf7cf7bdf7f4df2aa5cd7732afba44fc98c78e
Joakim Plate <elupus@ecce.se>
2019-07-01 13:32:57 +0200

    Allow 3.5 python

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license="MIT",
     packages=["arcam.fmj"],
     package_dir={"": "src"},
-    python_requires=">=3.6",
+    python_requires=">=3.5",
     author="Joakim Plate",
     install_requires=["asyncio", "attrs>18.1", "aionursery"],
     extras_require={
