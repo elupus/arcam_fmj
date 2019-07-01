57f2628458e9bc2e5fef88eeecd2f3b83089ff9d
Joakim Plate <elupus@ecce.se>
2019-07-02 00:40:03 +0200

    Drop marks that was only needed for pytest-asyncio

--- tests/test_fake.py
+++ tests/test_fake.py
@@ -29,7 +29,6 @@ async def run_context(loop, request, context):
     await context.__aenter__()
 
 
-@pytest.mark.asyncio
 @pytest.fixture
 async def server(loop, request):
     s = Server('localhost', 8888)
@@ -45,7 +44,7 @@ async def server(loop, request):
     )
     return s
 
-@pytest.mark.asyncio
+
 @pytest.fixture
 async def silent_server(loop, request):
     s = Server('localhost', 8888)
@@ -58,7 +57,7 @@ async def silent_server(loop, request):
     await run_context(loop, request, context)
     return s
 
-@pytest.mark.asyncio
+
 @pytest.fixture
 async def client(loop, request):
     c = Client("localhost", 8888, loop=loop)
@@ -66,19 +65,19 @@ async def client(loop, request):
     await run_context(loop, request, context)
     return c
 
-@pytest.mark.asyncio
+
 @pytest.fixture
 async def speedy_client(mocker):
     mocker.patch('arcam.fmj.client._HEARTBEAT_INTERVAL', new=timedelta(seconds=1))
     mocker.patch('arcam.fmj.client._HEARTBEAT_TIMEOUT', new=timedelta(seconds=2))
     mocker.patch('arcam.fmj.client._REQUEST_TIMEOUT', new=timedelta(seconds=0.5))
 
-@pytest.mark.asyncio
+
 async def test_power(loop, server, client):
     data = await client.request(0x01, CommandCodes.POWER, bytes([0xF0]))
     assert data == bytes([0x00])
 
-@pytest.mark.asyncio
+
 async def test_multiple(loop, server, client):
     data = await asyncio.gather(
         client.request(0x01, CommandCodes.POWER, bytes([0xF0])),
@@ -87,24 +86,24 @@ async def test_multiple(loop, server, client):
     assert data[0] == bytes([0x00])
     assert data[1] == bytes([0x01])
 
-@pytest.mark.asyncio
+
 async def test_invalid_command(loop, server, client):
     with pytest.raises(CommandNotRecognised):
         await client.request(0x01, 0xff, bytes([0xF0]))
 
-@pytest.mark.asyncio
+
 async def test_state(loop, server, client):
     state = State(client, 0x01)
     await state.update()
     assert state.get(CommandCodes.POWER) == bytes([0x00])
     assert state.get(CommandCodes.VOLUME) == bytes([0x01])
 
-@pytest.mark.asyncio
+
 async def test_silent_server_request(loop, speedy_client, silent_server, client):
     with pytest.raises(asyncio.TimeoutError):
         await client.request(0x01, CommandCodes.POWER, bytes([0xF0]))
 
-@pytest.mark.asyncio
+
 async def test_silent_server_disconnect(loop, speedy_client, silent_server):
     from arcam.fmj.client import _HEARTBEAT_TIMEOUT
 
@@ -116,7 +115,7 @@ async def test_silent_server_disconnect(loop, speedy_client, silent_server):
             connected = c.connected
     assert not connected
 
-@pytest.mark.asyncio
+
 async def test_heartbeat(loop, speedy_client, server, client):
     with asynctest.mock.patch.object(
             server,
