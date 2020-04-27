4c4537221200a7335a4d38af08c22216198b40b3
Joakim Plate <elupus@ecce.se>
2020-04-27 23:33:29 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="arcam-fmj",
-    version="0.4.4",
+    version="0.4.5",
     description="A python library for speaking to Arcam receivers",
     long_description=long_description,
     long_description_content_type="text/x-rst",
