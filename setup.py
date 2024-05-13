623659286510b72522d2ee0142fb010444e24c09
Joakim Plate <elupus@ecce.se>
2024-05-13 20:31:52 +0200

    Correct version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="arcam-fmj",
-    version="1.5.1",
+    version="1.5.2",
     description="A python library for speaking to Arcam receivers",
     long_description=long_description,
     long_description_content_type="text/x-rst",
